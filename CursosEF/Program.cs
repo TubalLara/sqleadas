d947e8b9920407039030eb2f459d6d16b71f27ea
TubalLara <tubal.lara@tajamar365.com>
2014-11-03 13:27:10 +0100

    primera subida

---
+++ CursosEF/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CursosEF.Modelo;
+
+namespace CursosEF
+{
+    class Program
+    {
+        static datosEntities db = new datosEntities();
+        private static void Main(string[] args)
+        {
+
+
+           
+           // var db = new datosEntities();//para poder acceder de otra manera
+                //esto siempre se conecta a la clase context, y lo que hace es decirle con qué base de datos quiero conectar
+
+
+            //var cur = new Curso()
+            // {
+            //   duracion =200,
+            // inicio = DateTime.Now,
+            //nombre = "Costura",
+            //profesor = 2
+            //          };
+
+            //db.Curso.Add(cur);
+            //db.SaveChanges();
+
+
+            //oreach (var curso in db.Curso)
+            //{
+            //  Console.WriteLine("curso: {0} empieza el {1:d} y lo imparte {2}", 
+            //curso.nombre,curso.inicio,curso.profesor.HasValue?curso.Profesor1.nombre:"Sin profesor");//lo de abajo que comento es lo mismo
+            // curso.Profesor1!=null?curso.Profesor1.nombre:"Sin profesor");
+
+
+            //curso.profesor = 3;
+
+            // db.Curso.Remove(curso);
+
+            //  }
+            // db.SaveChanges();
+
+
+            var curso = db.Curso.Find(2); //el Find busca por clave primaria
+
+            Console.WriteLine("curso: {0} empieza el {1:d} y lo imparte {2}",
+                curso.nombre, curso.inicio, curso.profesor.HasValue ? curso.Profesor1.nombre : "Sin profesor");
+                //lo de abajo que comento es lo mismo
+            //curso.Profesor1!=null?curso.Profesor1.nombre:"Sin profesor");
+
+            Console.ReadLine();
+
+        
+        
+        }
+
+        public IEnumerable<Curso> GetIdProfesor(int id)
+        {
+
+            var datos = db.Curso.Where(o => o.profesor == id).OrderBy(o => o.nombre);
+            //por cada elemento que hay dentro va a buscar alguno en el que el campo profesor sea igual que id    
+            /* var datos=from o in db.Curso
+                      where  o.profesor == id
+                      orderby o.nombre
+                      select o;*/
+
+            return datos;
+
+        }
+
+
+    }
+
+}
